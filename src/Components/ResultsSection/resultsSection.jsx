1dfdb35362e9b676713d3ddaf56b1092194a84a6
Soumik234 <115613165+Soumik234@users.noreply.github.com>
2023-08-15 21:06:24 +0530

    Search Updata

--- src/Components/ResultsSection/resultsSection.jsx
+++ src/Components/ResultsSection/resultsSection.jsx
@@ -1,33 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import './resultsSection.css'
-import Product from '../Product/product'
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState } from 'react';
+import './resultsSection.css';
+import Product from '../Product/product';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-function resultsSection({resultsArr, productsArr}) { 
+
+function ResultsSection({ resultsArr, productsArr,onSearch }) {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   
-  const navigate = useNavigate()
+  const filteredResults = resultsArr.filter((res) => {
+    if (!res.toolName) {
+      return false;
+    } else if (search === '') {
+      return true;
+    } else if (res.toolName.toLowerCase().includes(search.toLowerCase())) {
+      return true;
+    }
+    return false;
+  });
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://smartopia-backend.onrender.com/api/tool/search/${search}`
+      );
+      const data = response.data;
+      setSearchResults(data);
+      onSearch(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  
   return (
     <div className='resultsSection  border-bottom'>
+      {/* <SearchBar
+        setSearch={setSearch}
+        search={search}
+        // handleSearch={handleSearch}
+      /> */}
+
       <div className='results'>
-        {
-          resultsArr.map((res,i)=>(
-            <Product 
-                name={res.toolName}
-                key={res._id}
-                description={res.toolDesc}
-                pricingModel={res.priceModle}
-                imageURL={res.toolImageURL}
-                onClick={()=> navigate(`/productdetail/${res._id}`, {state: {productsArr, selectedTags:[]}})}
-              />
-          ))
-        }
+        {filteredResults.map((res) => (
+          <Product
+            name={res.toolName}
+            key={res._id}
+            description={res.toolDesc}
+            pricingModel={res.priceModle}
+            imageURL={res.toolImageURL}
+            onClick={() =>
+              navigate(`/productdetail/${res._id}`, {
+                state: { productsArr, selectedTags: [] },
+              })
+            }
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default resultsSection
+export default ResultsSection;
--- src/Components/SearchBar/searchBar.jsx
+++ src/Components/SearchBar/searchBar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 import './searchBar.css';
-function searchBar({setSearch, search}) {
+
+function SearchBar({ setSearch, search }) {
   return (
     <div className='searchBar'>
-        <input type="text" name="searchTearm" placeholder='Search' onChange={(e)=>setSearch(e.target.value)} value={search} />
-        <i className="searchIcon fa-solid fa-magnifying-glass"></i>
+      <input
+        type='text'
+        name='searchTerm'
+        placeholder='Search'
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      
+        <i className='searchIcon fa-solid fa-magnifying-glass'></i>
+      
     </div>
-  )
+  );
 }
 
-export default searchBar
+export default SearchBar;
+
