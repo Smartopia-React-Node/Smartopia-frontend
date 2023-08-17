03878b1f894efe77f9084364372281086961d1af
uklm10 <udayankhanra@gmail.com>
2023-08-17 12:57:09 +0530

    add some feature
    banner, admin glossary,bannerlist,dashboard,glossary,learn

--- src/Components/EditBanner/EditBanner.jsx
+++ src/Components/EditBanner/EditBanner.jsx
@@ -3,77 +3,86 @@ import "./editBanner.css";
 import Alert from '../Alert/Alert'
 import { Button, FormControl } from '@mui/material'
 import axios from 'axios';
-function EditBanner({setGlossaryArr}) {
+var Bid;
+function EditBanner() {
     const [alertType, setAlertType] = useState("nothing");
     const [newBannerText, set_newBannerText] = useState("");
     const [currentBannerText, set_currentBannerText] = useState("");
-    const getCurrentBannerText= async ()=>{
-        await axios.get("http://localhost:8800/api/banner/get")
-        .then((res)=> set_currentBannerText(res.data.text))
-        .catch((err)=> console.log(err))
+    const getCurrentBannerText = async (id) => {
+        // await axios.get("http://localhost:8800/api/banner/get")
+        await axios({
+            method: 'put',
+            url: `https://smartopia-backend.onrender.com/api/banner/use/${id}`
+        })
+            .then((res) => set_currentBannerText(res.data.text))
+            .catch((err) => console.log(err))
     }
     useEffect(() => {
-        getCurrentBannerText()
-    }, [])
-    
+        if (localStorage.getItem("bannerId")) {
+            Bid = localStorage.getItem("bannerId");
+            getCurrentBannerText(localStorage.getItem("bannerId"))
+        }
 
-    const handleGlossaryChange = (e)=>{
-        
+    }, [])
+
+
+    const handleGlossaryChange = (e) => {
+
         set_newBannerText(e.target.value)
         setAlertType("nothing");
     }
 
-    const updateBannerText =async ()=>{
-        if(newBannerText){
+    const updateBannerText = async (Bid) => {
+        if (newBannerText) {
             //update
-            await axios.put("http://localhost:8800/api/banner/update", {text: newBannerText})
-            .then(()=> setAlertType("succes"))
-            .catch((err)=> console.log(err))
+            await axios.put(`https://smartopia-backend.onrender.com/api/banner/edit/${Bid}`, { newBannerText })
+                .then(() => setAlertType("succes"))
+                .catch((err) => console.log(err))
 
             //get new one
             getCurrentBannerText()
-        }else{
+        } else {
             setAlertType("incomplete");
         }
     }
-  return (
-    <div className='add-glossary-container overlay'>
-        <Alert type={alertType}/>
-        <div className='header'>
-            <h2 >Edit Banner Text</h2>
-            <Button className='payment-btn' 
-                variant="contained"
-                onClick={updateBannerText}
+    return (
+        <div className='add-glossary-container overlay'>
+            <Alert type={alertType} />
+            <div className='header'>
+                <h2 >Edit Banner Text</h2>
+                <Button className='payment-btn'
+                    variant="contained"
+                    onClick={updateBannerText}
                 >
-                <span>Update</span>
-            </Button> 
-        </div>
-        <div className='glossary-upload-container'>
-            <div className='data-div'>
-                <FormControl >
-                    <div className='input-container'>
-                    <label>Current banner Text</label>
-                    <textarea type="text" 
-                            name="desc" 
-                            className="email glossary-desc edit-textarea"
-                            placeholder="Old banner Text"
-                            value={currentBannerText}
-                            disabled
-                        ></textarea>
+                    <span>Update</span>
+                </Button>
+            </div>
+            <div className='glossary-upload-container'>
+                <div className='data-div'>
+                    <FormControl >
+                        <div className='input-container'>
+                            <label>Current banner Text</label>
+                            <textarea type="text"
+                                name="desc"
+                                className="email glossary-desc edit-textarea"
+                                placeholder="Old banner Text"
+                                value={currentBannerText}
+                                disabled
+                            ></textarea>
 
-                        <label>New banner Text</label>
-                        <textarea type="text"  
-                            className="email glossary-desc edit-textarea"
-                            placeholder="Enter banner Text"
-                            onChange={handleGlossaryChange}
-                            value={newBannerText}
-                        ></textarea>
-                    </div>
-                </FormControl>
+                            <label>New banner Text</label>
+                            <textarea type="text"
+                                className="email glossary-desc edit-textarea"
+                                placeholder="Enter banner Text"
+                                onChange={handleGlossaryChange}
+                                value={newBannerText}
+                            ></textarea>
+                        </div>
+                    </FormControl>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default EditBanner
--- src/Pages/Admin/Glossary/glossay.jsx
+++ src/Pages/Admin/Glossary/glossay.jsx
@@ -6,23 +6,23 @@ import AddGlossary from '../../../Components/AddGlossary/addGlossary';
 import GlossaryAdminTable from '../../../Components/GlossaryAdminTable/glossaryAdminTable';
 import axios from 'axios';
 function glossay() {
-    const [glossaryArr, setGlossaryArr] = useState([])
+  const [glossaryArr, setGlossaryArr] = useState([])
 
-    useEffect(() => {
-        axios.get("https://ttool-test.onrender.com/api/glossary/all").then((response) => {
-            setGlossaryArr(response.data);
-          }).catch((err)=>console.log(err))
-    }, [])
+  useEffect(() => {
+    axios.get("https://ttool-test.onrender.com/api/glossary/all").then((response) => {
+      setGlossaryArr(response.data);
+    }).catch((err) => console.log(err))
+  }, [])
 
 
   return (
     <div className='glossary-admin-page'>
-        <Rightbar />
-        <div className='right-part'>
-          <TopBar />
-          <AddGlossary setGlossaryArr={setGlossaryArr}/>
-          <GlossaryAdminTable glossaryArr={glossaryArr}/>
-        </div>
+      <Rightbar />
+      <div className='right-part'>
+        <TopBar />
+        <AddGlossary setGlossaryArr={setGlossaryArr} />
+        <GlossaryAdminTable glossaryArr={glossaryArr} setGlossaryArr={setGlossaryArr} />
+      </div>
     </div>
   )
 }
